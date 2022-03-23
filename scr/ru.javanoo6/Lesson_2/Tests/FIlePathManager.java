48665e488dcacb2dbbca93a7fdc1c17f8e7a996b
javanoo6 <95355711+javanoo6@users.noreply.github.com>
2022-03-23 08:26:46 +0300

    исправлен FIlePathManager.java

--- scr/ru.javanoo6/Lesson_2/Tests/FIlePathManager.java
+++ scr/ru.javanoo6/Lesson_2/Tests/FIlePathManager.java
@@ -8,8 +8,8 @@ public class FIlePathManager {
 
     public enum FileType {xml, json}
 
-    public String DOM_XML_FILEPATH = "./DOMParer/DOM_XML_FILES/";
-    public String STAX_XML_FILEPATH = ".StaxParser/STAX_XML_Files/";
-    public String JSON_FILE_PATH = "./JSONParser/JSON_Files/";
+    public String DOM_XML_FILEPATH = "./scr/ru.javanoo6/Lesson_2/DOMParer/DOM_XML_FILES/";
+    public String STAX_XML_FILEPATH = "./scr/ru.javanoo6/Lesson_2/StaxParser/STAX_XML_Files/";
+    public String JSON_FILE_PATH = "./scr/ru.javanoo6/Lesson_2/JSONParser/JSON_Files/";
 
 }
